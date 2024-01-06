a5625bd2e7ac337e876992b52fdf0662eb06dbbd
edcalderin <edcm.erick@gmail.com>
2024-01-06 14:22:58 -0500

    handling exceptions in utils

--- backend_app/src/utils.py
+++ backend_app/src/utils.py
@@ -1,6 +1,8 @@
-import pickle
+import json
 import logging
 from pathlib import Path
+import pickle
+import sys
 
 ROOT_DIR = Path(__file__).parent.parent
 
@@ -12,7 +14,22 @@ class Utils:
             MODEL_PATH: Path = ROOT_DIR.joinpath('iris_model.pkl')
             with open(MODEL_PATH, 'rb') as file:
                 model_pkl = pickle.load(file)
-            return model_pkl
-    
+
         except FileNotFoundError as e:
-            logging.error(f'The model {MODEL_PATH} was not found')
+            exc_type, _, exc_traceback = sys.exc_info()
+            
+            exception_details = {
+                'exc_type': exc_type.__name__,
+                'filename': exc_traceback.tb_frame.f_code.co_filename,
+                'module': exc_traceback.tb_frame.f_code.co_name,
+                'line_number': exc_traceback.tb_lineno,
+                'exception': str(e)
+            }
+            
+            logging.error(json.dumps(exception_details, indent = 3))
+        
+        except Exception as e:
+            logging.error(e)
+            
+        else:
+            return model_pkl
