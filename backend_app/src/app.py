68528d82651c2e0edacd724a16b074a4db9a4412
edcalderin <edcm.erick@gmail.com>
2024-01-06 14:46:47 -0500

    loading model in the app startup

--- backend_app/src/app.py
+++ backend_app/src/app.py
@@ -1,13 +1,25 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 import uvicorn
 from backend_app.src.schemas.iris_input import IrisInput
 from backend_app.src.controllers.model_controller import ModelController
+from backend_app.src.utils import Utils
+import logging
 
-app = FastAPI()
+resource = {}
+
+@asynccontextmanager
+async def app_lifestpan(_: FastAPI):
+    logging.info('Loading model')
+    resource['model'] = Utils.load_model()
+    yield
+    resource.clear()
+
+app = FastAPI(lifespan = app_lifestpan)
 
 @app.post('/predict')
 def predict(input: IrisInput):
-    controller = ModelController()
+    controller = ModelController(model = resource['model'])
     return controller.predict(input)
 
 if __name__ == '__main__':
